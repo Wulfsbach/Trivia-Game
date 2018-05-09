c6ee14f8ee5408ca6c643fffa58fb3e1a4607546
Wulfsbach <etemelkos@gmail.com>
2018-05-09 15:11:53 -0400

    working on it

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,42 @@
+
+
+//variables delcaration
+
+var time = 15;
+var correctAns = 0;
+var IncorrectAns=0;
+var current=0;
+
+
+
+
+
+$(document).ready(function(){
 var questions =[{
-    q1: "Which House Does not Belong?",
-    ans1:["Gryfindor", "Slytherin", "Hufflepuff","Eagleclaw"],
+    q: "Which House Does not Belong?",
+    ans:["Gryfindor", "Slytherin", "Hufflepuff","Eagleclaw"],
 corrrect: 4,
 },{
-    q2: "Who is the headmaster at Hogwarts?" ,    
+    q: "Who is the headmaster at Hogwarts?" ,    
     ans2: ["Dusseldorf", "Gandalf", "Dumbledore","Hodor"],
     correct: 3,
 },{
-q3: "Who is the main villain in the Series?",
-ans3:["Draco Malfoy", "Snape", "Voldemort","Stalin"],
+q: "Who is the main villain in the Series?",
+ans:["Draco Malfoy", "Snape", "Voldemort","Stalin"],
 correct: 3,
 },{
-    q4:"What was Harry Potter's owl named?",
-   ans4: ["Hedwig", "Melvin", "Fluffy", "Owl"],
+    q:"What was Harry Potter's owl named?",
+   ans: ["Hedwig", "Melvin", "Fluffy", "Owl"],
 correct: 1,
 }];
-var slime =
+
+console.log(questions[0]);
 
 
 function renderQuestion(){
+var quiz= questions[current].q;
+
+};
 
 
-}
+})
