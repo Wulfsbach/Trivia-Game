90664a4e61d2aac70ce7e61fdae07ca396cf571e
Wulfsbach <etemelkos@gmail.com>
2018-05-09 14:02:58 -0400

    New

---
+++ assets/javascript/app.js
@@ -0,0 +1,24 @@
+var questions =[{
+    q1: "Which House Does not Belong?",
+    ans1:["Gryfindor", "Slytherin", "Hufflepuff","Eagleclaw"],
+corrrect: 4,
+},{
+    q2: "Who is the headmaster at Hogwarts?" ,    
+    ans2: ["Dusseldorf", "Gandalf", "Dumbledore","Hodor"],
+    correct: 3,
+},{
+q3: "Who is the main villain in the Series?",
+ans3:["Draco Malfoy", "Snape", "Voldemort","Stalin"],
+correct: 3,
+},{
+    q4:"What was Harry Potter's owl named?",
+   ans4: ["Hedwig", "Melvin", "Fluffy", "Owl"],
+correct: 1,
+}];
+var slime =
+
+
+function renderQuestion(){
+
+
+}
